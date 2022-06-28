7c1d9ea7b7b737bfdf728b20e1b14b2166919efa
webhav <46425617+pooniavaibhav@users.noreply.github.com>
2022-06-28 16:12:25 +0530

    Add files via upload

---
+++ Recursion.py
@@ -0,0 +1,24 @@
+"""
+Recursion- 
+A way of solving a problem by having a function calling itself.
+Property-
+- Performing the same operation multiple times with different inputs.
+- In every step we try smaller inputs to make the problem smaller.
+- Base condition is needed to stop the recursion, otherwise infinite loop will occur.
+
+Where to use-
+If you can divide the problem into smaller sub problems or statement seems like -
+- Design an algorithm to compute nth...
+- Write code to list the n...
+- Implement a method to compute all.
+"""
+
+"""
+Implementation -
+def recursionmethod(parameters):
+    if exit from condition satidfied:
+        return some value
+    else:
+        recursionmethod(modified paramters
+"""
+
